fc36d009791ada4685f8dc29b68665cd49ff7927
santoshml-lab <santoshkrsbg36@gmail.com>
2026-04-22 21:34:07 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,36 +1,44 @@
-from fastapi import FastAPI
-import pandas as pd
-import joblib
 
-app = FastAPI()
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import pandas as pd
 
 # LOAD MODEL + COLUMNS
 model = joblib.load("xgb_model.pkl")
 cols = joblib.load("columns.pkl")
 
+app = FastAPI()
+
+# INPUT SCHEMA (UI/POSTMAN)
+class InputData(BaseModel):
+    amt: float
+    category: str
+    gender: str
+    city_pop: int
+
 @app.get("/")
 def home():
-    return {"message": "Credit Default API Running 🚀"}
+    return {"message": "Fraud Detection API is Live 🚀"}
 
 @app.post("/predict")
-def predict(data: dict):
-    try:
-        # input → dataframe
-        df = pd.DataFrame([data])
+def predict(data: InputData):
 
-        # encoding (same as training)
-        df = pd.get_dummies(df)
+    # convert input to dataframe
+    df = pd.DataFrame([data.dict()])
 
-        # align columns
-        df = df.reindex(columns=cols, fill_value=0)
+    # one-hot encoding (same as training)
+    df = pd.get_dummies(df)
 
-        # prediction
-        prob = model.predict_proba(df)[0][1]
+    # align with training columns (🔥 IMPORTANT FIX)
+    df = df.reindex(columns=cols, fill_value=0)
 
-        return {
-            "probability": float(prob),
-            "decision": "High Risk ⚠️" if prob > 0.5 else "Low Risk ✅"
-        }
+    # prediction
+    pred = model.predict(df)[0]
+    prob = model.predict_proba(df)[0][1]
 
-    except Exception as e:
-        return {"error": str(e)}
+    return {
+        "prediction": int(pred),
+        "probability": float(prob),
+        "risk": "HIGH" if prob > 0.5 else "LOW"
+    }
