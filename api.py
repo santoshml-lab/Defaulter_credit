dc1086ea4f52e0373a3a284f18661e7a961c7a35
santoshml-lab <santoshkrsbg36@gmail.com>
2026-04-23 12:31:41 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,44 +1,49 @@
-
 from fastapi import FastAPI
 from pydantic import BaseModel
-import joblib
 import pandas as pd
+import numpy as np
+import joblib
 
-# LOAD MODEL + COLUMNS
 model = joblib.load("xgb_model.pkl")
 cols = joblib.load("columns.pkl")
 
 app = FastAPI()
 
-# INPUT SCHEMA (UI/POSTMAN)
 class InputData(BaseModel):
     amt: float
     category: str
     gender: str
     city_pop: int
-
-@app.get("/")
-def home():
-    return {"message": "Fraud Detection API is Live 🚀"}
+    lat: float
+    long: float
+    merch_lat: float
+    merch_long: float
 
 @app.post("/predict")
 def predict(data: InputData):
 
-    # convert input to dataframe
     df = pd.DataFrame([data.dict()])
 
-    # one-hot encoding (same as training)
+    # 🔥 create distance same as training
+    df["distance"] = np.sqrt(
+        (df["lat"] - df["merch_lat"])**2 +
+        (df["long"] - df["merch_long"])**2
+    )
+
+    df = df.drop(columns=["lat","long","merch_lat","merch_long"])
+
+    # encoding
     df = pd.get_dummies(df)
 
-    # align with training columns (🔥 IMPORTANT FIX)
+    # align
     df = df.reindex(columns=cols, fill_value=0)
 
-    # prediction
     pred = model.predict(df)[0]
     prob = model.predict_proba(df)[0][1]
 
     return {
         "prediction": int(pred),
         "probability": float(prob),
-        "risk": "HIGH" if prob > 0.5 else "LOW"
+        "risk": "HIGH" if prob > 0.4 else "LOW"
     }
+     
